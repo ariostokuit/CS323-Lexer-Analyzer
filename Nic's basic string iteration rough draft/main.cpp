9de7452626e7922d671dbbc72534cc5843a418b9
Nicholas Webster <43260193+NicWebster96@users.noreply.github.com>
2019-09-18 13:40:01 -0700

    Update main.cpp

--- Nic's basic string iteration rough draft/main.cpp
+++ Nic's basic string iteration rough draft/main.cpp
@@ -2,75 +2,82 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-string parse(string str1) {
+vector<string> parse(string str1) {
 
   // Didn't feel like writing the input file function yet
 
-  int indexStart = 0
+  vector<string> pList;
+  int indexStart = 0;
   int indexEnd = 0;
-  int count = 1;
   bool loopBreak = false;
 
   // ensure index[0] is a letter in this test case
-  cout << "\nstr1[index] = " << str1[indexStart] << endl;
+  cout << "\n\nstr1[index] = " << str1[indexStart] << endl;
 
   if(isalpha(str1[indexStart]) && loopBreak == false) {
     for(int i=0; i<str1.length(); i++) {
-      if(isalpha(str1[indexEnd+1]) && loopBreak==false) {
+      if(isalpha(str1[indexEnd+1]) || isdigit(str1[indexEnd+1]) || str1[indexEnd+1] == '$' && loopBreak==false) {
         indexEnd++;
-        count++;
       } else {
         loopBreak = true;
         // all these cout statements are for testing
         cout << "indexStart is " << indexStart << endl;
-        cout << "indexEnd is " << indexEnd << endl;
-        cout << "count is " << count << endl;
-        cout << "The substr is... " << str1.substr(indexStart, count) << endl;
+        cout << "indexEnd is " << indexEnd << "\n\n";
+        cout << "The substr is... " << str1.substr(indexStart, indexEnd+1) << endl;
+
+        cout << "Adding substring to parsedList vector...\n";
+        pList.push_back(str1.substr(indexStart, indexEnd+1));
+
         cout << "Deleting this substr from the string...\n";
-        str1.erase(indexStart, count);
-        cout << "New str1 is... " << str1;
-        return str1;
+        str1.erase(indexStart, indexEnd+1);
+        cout << "New str1 is: " << str1 << "\n";
+        return pList;
       }
     }
   }
+  return pList;
 
   //Can use the substr to pull the word from the str1.
   //From here, indexStart becomes indexEnd + 1. Rinse and repeat.
-  //or...
-  //return the str after removing the substr and use a while loop in main
-  //    with the condition while(str!=empty)
-
-  if(str1[indexStart] == '(' && ')') {
-    cout << "seperator!\n";
-    return str1;
-  }
-
-  if(isdigit(str1[indexStart])) {
-    cout << "digit!\n";
-    return str1;
-  }
-
-  if(isblank(str1[indexStart])) {
-    cout << "blank!\n";
-    return str1;
-  }
+  //
+  // if(str1[indexStart] == '(' && ')') {
+  //   cout << "seperator!\n";
+  //   return str1;
+  // }
+  //
+  // if(isdigit(str1[indexStart])) {
+  //   cout << "digit!\n";
+  //   return str1;
+  // }
+  //
+  // if(isblank(str1[indexStart])) {
+  //   cout << "blank!\n";
+  //   return str1;
+  // }
 
 }
 
 int main() {
 
-  string a = "while (fahr < upper) a = 23.00 whileend\n";
+  vector<string> parsedList;
+
+  string a = "wljsdgl34234$sdf4$(fahr < upper) a = 23.00 whileend\n";
   string b = "(operator test case)\n";
   string c = "23 digit first test case\n";
   string d = " starts with a blank\n";
 
-  cout << parse(a);
-  cout << parse(b);
-  cout << parse(c);
-  cout << parse(d);
+  parsedList = parse(a);
+  // cout << parse(b);
+  // cout << parse(c);
+  // cout << parse(d);
+
+  cout << "parsedList contains: ";
+  cout << parsedList[0] << "\n\n";
+  cout << "parsedList is size " << parsedList.size() << "\n\n";
 
   return 0;
 }
