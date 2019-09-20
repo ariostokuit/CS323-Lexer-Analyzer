50b13eedf8b8694442de8ab41788529175cac900
Nicholas Webster <43260193+NicWebster96@users.noreply.github.com>
2019-09-20 15:49:07 -0700

    completed parser

--- Nic's basic string iteration rough draft/main.cpp
+++ Nic's basic string iteration rough draft/main.cpp
@@ -13,19 +13,17 @@ const vector<string> OPERATOR_LIST = { "*", "+", "-", "=", "/", ">", "<", "%" };
 void parseLetter(string &str, vector<string> &pL) {
   /*
     parseLetter checks for letters, numbers, and dollar signs.
-    All three are valid for a variable name.
+    All three are valid for an identifier name.
     Once i+1 is not one of those three, it pushes the substring to the vector
       which was passed by reference.
-    The FSM will later determie whether the string is a keyword or variable name
+    The FSM will later determie whether the string is a keyword or an identifier
   */
   for(int i=0; i<str.length(); i++) {
     if(isalpha(str[i+1]) || isdigit(str[i+1]) || str[i+1] == '$') {
       continue;
     } else {
       pL.push_back(str.substr(0, i+1));
-      cout << "\n\nDeleting letter: " << str.substr(0, i+1);
       str.erase(0, i+1);
-      cout << "\nReturning to parse() funct\n";
       return;
     }
   }
@@ -34,7 +32,7 @@ void parseLetter(string &str, vector<string> &pL) {
 void parseNumber(string &str, vector<string> &pL) {
   /*
     parseNumber allows for digits and only ONE period.
-    If there's more than 1 period, cout warning and continue running.
+    If there's more than one period, cout a warning, but continue to run.
     The FSM will later determine if it is a real or integer.
   */
   int periodCount = 0;
@@ -47,7 +45,6 @@ void parseNumber(string &str, vector<string> &pL) {
       if(periodCount > 1) {cout << "\n\nWARNING: MORE THAN ONE PERIOD DETECTED!\n";}
     } else {
       pL.push_back(str.substr(0, i+1));
-      cout << "\n\nDeleting number: " << str.substr(0, i+1);
       str.erase(0, i+1);
       return;
     }
@@ -58,70 +55,60 @@ vector<string> parse(string str1) {
 
   vector<string> pList;
   string atZero;
-  string newLineCheck;
+  atZero.assign(str1, 0, 1);
 /*DELETE THIS COMMENT BEFORE FINAL SUBMISSION
   I wasn't able to compare str1[0] == SEPARATORS_LIST[i]. That requires
   overloading the == operator (I think) and I didn't feel like doing that.
   So I created the 'atZero' string and set it equal to the first
   character in the string. Pretty janky but it works. Feel free to edit it
 
-  Also, if there is more than one period in a number entry, it will give a
+  If there is more than one period in a number entry, it will give a
   warning but continue to run. You can edit this if you like.
 */
   do {
-
-  cout << "\nAssigning new atZero\n";
-  atZero.assign(str1, 0, 1);
-  newLineCheck.assign(str1, 0, 2);
-
   ////////// IF LETTER //////////
   if(isalpha(str1[0])) {
     parseLetter(str1, pList);
-    cout << "\nDone deleting letter";
+    atZero.assign(str1, 0, 1);
   }
 
   ////////// IF NUMBER //////////
   if(isdigit(str1[0])) {
     parseNumber(str1, pList);
-    cout << "\nDone deleting number";
+    atZero.assign(str1, 0, 1);
   }
 
-  ////////// IF WHITESPACE //////////
-  if(str1[0] == ' ') {
-    cout << "\n\nDeleting ws " << str1.substr(0, 1);
+  ////////// IF WHITESPACE -> ERASE //////////
+  if(isblank(str1[0])) {
     str1.erase(0, 1);
-    cout << "\nDone deleting ws";
+    atZero.assign(str1, 0, 1);
   }
 
-  ////////// IF COMMENT //////////
+  ////////// IF COMMENT -> ERASE ENTIRE COMMENT //////////
   if(str1[0] == '!') {
     for(int i = 1; i<str1.size(); i++) {
       if(str1[i] == '!') {
-        cout << "\n\nDeleting comment: " << str1.substr(0, i+1);
         str1.erase(0, i+1);
+        atZero.assign(str1, 0, 1);
       }
     }
-    cout << "\nDone deleting comment";
   }
 
-  ////////// IF NEWLINE //////////
-  // if(newLineCheck == '\n') {
-  //   str1.erase(0, 2);
-  //   cout << "\nDone deleting comment!\n";
-  // }
-
-  ////////// IF SEPARATOR OR OPERATOR //////////
+  ////////// IF SEPARATOR //////////
   for(int i = 0; i<SEPARATORS_LIST.size(); i++) {
     if(atZero == SEPARATORS_LIST[i]) {
       pList.push_back(str1.substr(0, 1));
-      cout << "\n\nDeleting sep: " << str1.substr(0, 1);
       str1.erase(0, 1);
-      cout << "\nDone deleting sep";
-    } else if(atZero == OPERATOR_LIST[i]) {
+      atZero.assign(str1, 0, 1);
+    }
+  }
+
+  ////////// IF OPERATOR //////////
+  for(int i = 0; i<OPERATOR_LIST.size(); i++) {
+     if(atZero == OPERATOR_LIST[i]) {
       pList.push_back(str1.substr(0, 1));
-      cout << "\n\nDeleting op: " << str1.substr(0, 1) << "\n\n";
       str1.erase(0, 1);
-      cout << "\nDone deleting op";
+      atZero.assign(str1, 0, 1);
     }
   }
 
@@ -138,7 +125,8 @@ string readFile(string fname){
     if(myFile.is_open()){ //open the file
       while(!myFile.eof()) //continue iterating until end of file
       {
-        getline(myFile,temp,'\0'); //store each line of code in a temp variable
+        getline(myFile, temp); //store each line of code in a temp variable
+        temp += " ";
         code += temp; //concatenate the code into one string
       }
     } else {
@@ -149,16 +137,13 @@ string readFile(string fname){
 
 int main() {
 
+  vector<string> parsedList;
   string file = "Code.txt";
   string code = readFile(file);
-  cout << code;
 
-  vector<string> parsedList;
-  string a = "gl34234$sdf4$(fahr < upper) a = 23.00 whileend";
-  //delete this^^
   parsedList = parse(code);
 
-  cout << "\nparsedList contains: \n";
+  cout << "\nparsedList contains: \n\n";
   for(int i = 0; i<parsedList.size(); i++) {
       cout << parsedList[i] << "\n";
   }
